d90e2f8ce5c1a0cba2b73da96fb1d036d4e885eb
Christopher Turtle <chris@onapp.com>
2012-12-26 10:24:56 +0000

    added basic game

--- lib/trifik/engine/game.rb
+++ lib/trifik/engine/game.rb
@@ -1,5 +1,21 @@
 class Game
   
-  attr_accessor :interpreter, :executive
+  attr_reader :interpreter, :executive, :input_device, :output_device
+  
+  def initialize(interpreter, executive, input_device=STDIN, output_device=STDOUT)
+    @interpreter = interpreter
+    @executive = executive
+    @input_device = input_device
+    @output_device = output_device
+  end
+  
+  def play
+    input = input_device.gets
+    command = interpreter.interpret(input)
+    instruction = command[:command]
+    args = command[:args]
+    output = executive.instance_exec(*args, &instruction)
+    output_device.puts output
+  end
   
 end
--- spec/engine/game_spec.rb
+++ spec/engine/game_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
-describe Game do
-  let(:game)        { subject }
-  let(:interpreter) { mock 'interpreter' }
-  let(:executive)   { mock 'executive' }
-  
-  describe "#interpeter" do
-    it "has one" do
-      game.interpreter = interpreter
-      game.interpreter.should == interpreter
-    end
+class MockExecutive
+
+  def say_hi(name)
+    "hai yourself, #{name}"
   end
-  
-  describe "#executive" do
-    it "has one" do
-      game.executive = executive
-      game.executive.should == executive
+end
+
+describe Game do
+  let(:interpreter) { mock 'interpreter' }
+  let(:executive)   { MockExecutive.new }
+  let(:command)     { {
+                        command: proc {|name| say_hi(name) }, args: ["johnny"]
+                    } }
+                    
+  let(:game)        { Game.new(interpreter, executive) }
+
+  describe "#play" do
+    it "gets an input, inteprets, executes and outputs the result" do
+      game.input_device.should_receive(:gets).and_return("hai johnny")
+      game.interpreter.should_receive(:interpret).with("hai johnny").and_return(command)
+      game.output_device.should_receive(:puts).with("hai yourself, johnny")
+      game.play
     end
   end
   
